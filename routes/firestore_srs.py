2cfeabce2d61cf264fa4c3e1c8babf06695280b8
Dario Macboock <a01785420@tec.mx>
2026-03-10 11:32:54 -0600

    fix conexión agente y firestore

--- routes/firestore_srs.py
+++ routes/firestore_srs.py
@@ -3,15 +3,31 @@ from google.cloud import firestore
 from pydantic import BaseModel
 from dotenv import load_dotenv
 import os
+from google.oauth2 import service_account
+
+load_dotenv()
+
 
 load_dotenv()
 
 FIRESTORE_PROJECT = os.getenv("FIRESTORE_PROJECT")
-COLLECTION        = os.getenv("FIRESTORE_COLLECTION", "documentos")
-DOC_ID            = "DDYWBQOZG2WYrHrs4a3e"
+COLLECTION = os.getenv("FIRESTORE_COLLECTION", "documentos")
+DOC_ID = "DDYWBQOZG2WYrHrs4a3e"
 
+# cargar ruta de credenciales
+FIRESTORE_CREDENTIALS_PATH = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_FIRESTORE")
+
+# crear credenciales
+credentials = service_account.Credentials.from_service_account_file(
+    FIRESTORE_CREDENTIALS_PATH
+)
+
+# crear cliente firestore con credenciales explícitas
+_db = firestore.Client(
+    project=FIRESTORE_PROJECT,
+    credentials=credentials
+)
 router = APIRouter(prefix="/firestore", tags=["firestore"])
-_db = firestore.Client(project=FIRESTORE_PROJECT)
 
 
 class DocumentoPayload(BaseModel):
