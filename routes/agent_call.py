98076659a345bc955e14e29d0b788e593a178735
Dario Macboock <a01785420@tec.mx>
2026-03-10 20:06:18 -0600

    nuevo agente

--- routes/agent_call.py
+++ routes/agent_call.py
@@ -7,7 +7,7 @@ from fastapi import APIRouter, HTTPException, Depends
 # Configuration
 PROJECT_ID = "anemona-2130e"
 LOCATION = "us-central1"
-RESOURCE_ID = "7350454034496487424"
+RESOURCE_ID = "2710057568444612608"
                
 AGENT_RESOURCE_NAME = f"projects/{PROJECT_ID}/locations/{LOCATION}/reasoningEngines/{RESOURCE_ID}"
 
