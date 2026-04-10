00664b5837b9505eab927088fc98c81813f0b621
Santiago Cordova <A01029211@tec.mx>
2026-04-10 14:26:01 -0600

    log in funciones

--- routes/login_route.py
+++ routes/login_route.py
@@ -14,24 +14,51 @@ class DatosRegistro(BaseModel):
     email: str
     password: str
 
-#@router.post("/register")  # ← app. cambia a router.
-#def registrar_usuario(datos: DatosRegistro):
-#    if datos.email in usuarios_db:
-#        raise HTTPException(status_code=400, detail="Este email ya está registrado")
-#    usuarios_db[datos.email] = hashear_password(datos.password)
-#    return {"mensaje": "Usuario creado exitosamente"}
-
 @router.post("/login")  # ← app. cambia a router.
 def login(form: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     usuario = db.query(Usuario).filter(Usuario.correo == form.username).first()
     if not usuario or not verificar_password(form.password, usuario.password):
         raise HTTPException(status_code=401, detail='Correo o contraseña incorrectos')
-    token = crear_token(form.username)
-    return{"access_token": token, "token_type": "bearer"}
+    
+    usuario.ultimoacceso = datetime.utcnow()
+    db.commit()
+    
+    
+    token = crear_token(usuario.correo, usuario.idusuario)
 
-@router.get("/me")  # ← app. cambia a router.
-def obtener_mi_info(token: str = Depends(oauth2_scheme)):
-    correo = leer_token(token)
-    if not correo:
+    return{
+        "access_token": token, 
+        "token_type": "bearer",
+        "idusuario": usuario.idusuario,
+        "nombre": usuario.nombre,
+        "apellidopaterno": usuario.apellidopaterno,
+        "apellidomaterno": usuario.apellidomaterno,
+        "correo": usuario.correo,
+        "ultimoacceso": usuario.ultimoacceso,
+        "activo": usuario.activo,
+        "iddepartamento": usuario.iddepartamento,
+        "idrol": usuario.idrol
+        }
+
+@router.get("/me")
+def obtener_mi_info(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    datos = leer_token(token)
+    if not datos:
         raise HTTPException(status_code=401, detail="Token inválido o expirado")
-    return {"correo": correo}
+
+    # Busca el usuario fresco de la BD con todos sus datos
+    usuario = db.query(Usuario).filter(Usuario.correo == datos["correo"]).first()
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+    return {
+        "idusuario":          usuario.idusuario,
+        "nombre":             usuario.nombre,
+        "apellidopaterno":    usuario.apellidopaterno,
+        "apellidomaterno":    usuario.apellidomaterno,
+        "correo":             usuario.correo,
+        "ultimoacceso":       usuario.ultimoacceso,
+        "activo":             usuario.activo,
+        "iddepartamento":     usuario.iddepartamento,
+        "idrol":              usuario.idrol
+    }
