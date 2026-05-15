b05f3680b3ae826ffcaeca7e729bb703cd96ee72
Santiago Cordova <A01029211@tec.mx>
2026-05-14 20:47:40 -0600

    arreglar https

--- routes/email_route.py
+++ routes/email_route.py
@@ -24,7 +24,7 @@ from utils.auth import leer_token
 from database import get_db
 from models import Usuario
 
-FRONTEND_URL = "anemona-backend-fireabse--anemona-2130e.us-east4.hosted.app"
+FRONTEND_URL = "https://anemona-backend-fireabse--anemona-2130e.us-east4.hosted.app"
 
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
