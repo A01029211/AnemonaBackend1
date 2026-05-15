2c56d1a0f4befe55bf78ce167bcbc9238002ba73
Dario Macboock <a01785420@tec.mx>
2026-05-15 16:35:11 -0600

    nuevo agente con wisget nuevo

--- routes/agent_call.py
+++ routes/agent_call.py
@@ -8,7 +8,7 @@ import json
 # Configuration
 PROJECT_ID = "anemona-2130e"
 LOCATION = "us-central1"
-RESOURCE_ID = "5294786754241363968"
+RESOURCE_ID = "7458484900216176640"
                
 AGENT_RESOURCE_NAME = f"projects/{PROJECT_ID}/locations/{LOCATION}/reasoningEngines/{RESOURCE_ID}"
 
--- routes/firestore_srs.py
+++ routes/firestore_srs.py
@@ -43,7 +43,7 @@ _db = firestore.Client(
 
 PROJECT_ID = "anemona-2130e"
 LOCATION = "us-central1"
-RESOURCE_ID = "5294786754241363968"
+RESOURCE_ID = "7458484900216176640"
 
 AGENT_RESOURCE_NAME = f"projects/{PROJECT_ID}/locations/{LOCATION}/reasoningEngines/{RESOURCE_ID}"
 
