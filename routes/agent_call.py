51e89d8ae0f202b8263a35e3846f97eed9f439f2
Dario Macboock <a01785420@tec.mx>
2026-05-17 18:09:47 -0600

    agente actualizado 3.9

--- routes/agent_call.py
+++ routes/agent_call.py
@@ -8,7 +8,7 @@ import json
 # Configuration
 PROJECT_ID = "anemona-2130e"
 LOCATION = "us-central1"
-RESOURCE_ID = "8497127564278497280"
+RESOURCE_ID = "280274859768938496"
                
 AGENT_RESOURCE_NAME = f"projects/{PROJECT_ID}/locations/{LOCATION}/reasoningEngines/{RESOURCE_ID}"
 
